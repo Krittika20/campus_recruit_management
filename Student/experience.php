c501096a717da98815d36c77b625ef251d2a69b1
puneetha10 <68530838+puneetha10@users.noreply.github.com>
2020-10-23 19:38:05 +0530

    Update experience.php

--- Student/experience.php
+++ Student/experience.php
@@ -137,6 +137,14 @@ if(array_key_exists("submit",$_POST))
                 </ul>
             </div>
             </nav>
+	    <div id="page-wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">Tables</h1>
+                        </div>
+                        <!-- /.col-lg-12 -->
+                    </div>
             <div class="row">
                         <div class="col-lg-12">
                             <div class="panel panel-default">
